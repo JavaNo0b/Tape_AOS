cc90d6af8c7d2c22f173237d0ec56ba841328124
sumin1012 <ksm2521101@naver.con>
2024-01-15 21:43:39 +0900

    로그인, 알림

---
+++ app/src/main/java/com/janob/tape_aos/NotifFragment.kt
@@ -0,0 +1,28 @@
+package com.janob.tape_aos
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.janob.tape_aos.databinding.FragmentNotifBinding
+
+
+class NotifFragment : Fragment() {
+    lateinit var binding: FragmentNotifBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNotifBinding.inflate(inflater, container, false)
+
+        val notifRVAdapter = NotifRVAdapter()
+        binding.notifRv.adapter=notifRVAdapter //리사이클러뷰 어댑터와 연결
+        binding.notifRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        return binding.root
+    }
+}

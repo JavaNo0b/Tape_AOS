264e8975439f923bcad0cd5c9e055aa563e4088c
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-01-16 19:42:32 +0900

    메뉴바 구현(위치조정 실패)

--- app/src/main/java/com/janob/tape_aos/ReplyAdapter.kt
+++ app/src/main/java/com/janob/tape_aos/ReplyAdapter.kt
@@ -1,54 +1,73 @@
 package com.janob.tape_aos
 
+import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.PopupWindow
 import androidx.recyclerview.widget.RecyclerView
+import com.janob.tape_aos.databinding.ItemReplyBinding
 
-class ReplyAdapter(val replies : List<Reply>, val activity: ReplyActivity) : RecyclerView.Adapter<ReplyAdapter.ReplyItemViewHolder>() {
+class ReplyAdapter(private val replies : List<Reply>, private val context: Context) : RecyclerView.Adapter<ReplyAdapter.ReplyItemViewHolder>() {
 
     private val dataList = replies
 
-    class ReplyItemViewHolder(val view : View, val activity: ReplyActivity) : RecyclerView.ViewHolder(view)
+    inner class ReplyItemViewHolder(val binding : ItemReplyBinding) : RecyclerView.ViewHolder(binding.root)
     {
         lateinit var reply : Reply
-        init{
 
-            //클릭한 댓글만 수정
-            view.setOnClickListener{
-                val intent = Intent(it.context,ReplyModifyActivity::class.java)
-                intent.putExtra("reply",reply)
-
-                activity.startActivity(intent)
-
-            }
-        }
+        //댓글 바인딩
         fun bind(reply : Reply){
             this.reply = reply
 
-            var text = view.findViewById<TextView>(R.id.reply_text_tv)
+            var text = binding.replyTextTv
             text.text = reply.text
 
+        }
+        //replyEditBtn 클릭 시 menu
+        init {
+            binding.replyEditBtn.setOnClickListener {
+                showPopup(it)
+            }
+        }
+
+        //menu popup
+        private fun showPopup(view: View) {
+            val inflater = view.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val popupView = inflater.inflate(R.layout.custom_reply_popup_menu, null)
+
+            val width = 163.dpToPx(context)
+            val height = 61.dpToPx(context)
+
+            val popupWindow = PopupWindow(popupView, width, height, true)
+            popupWindow.showAsDropDown(view, 0, 0)
+
+        }
+
+        //menu popup 사이즈 변경
+        fun Int.dpToPx(context: Context): Int {
+            return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                this.toFloat(),
+                context.resources.displayMetrics
+            ).toInt()
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReplyItemViewHolder {
-        //item view
-        val view = LayoutInflater.from(parent.context).inflate(viewType,parent,false)
-        return ReplyItemViewHolder(view,activity)
+        val binding : ItemReplyBinding = ItemReplyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ReplyItemViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return dataList.size
-    }
+    override fun getItemCount(): Int = dataList.size
 
     override fun onBindViewHolder(holder: ReplyItemViewHolder, position: Int) {
         holder.bind(dataList[position])
-    }
 
-    override fun getItemViewType(position: Int): Int {
-        return R.layout.item_reply
     }
 }
--- app/src/main/java/com/janob/tape_aos/TapeAlbumRVAdapter.kt
+++ app/src/main/java/com/janob/tape_aos/TapeAlbumRVAdapter.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupWindow
 import androidx.recyclerview.widget.RecyclerView
+import com.janob.tape_aos.databinding.CustomReplyPopupMenuBinding
 import com.janob.tape_aos.databinding.ItemTapeBinding
 
 

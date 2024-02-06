26ba228fcd185c60a00927931ad1aac16e314a0e
sumin1012 <ksm2521101@naver.con>
2024-02-07 01:00:53 +0900

    이전코드

--- app/src/main/java/com/janob/tape_aos/NotifFragment.kt
+++ app/src/main/java/com/janob/tape_aos/NotifFragment.kt
@@ -28,33 +28,3 @@ class NotifFragment : Fragment() {
     }
 
 }
-/*
-class NotifFragment : Fragment() {
-
-    lateinit var binding: FragmentNotifBinding
-    lateinit var NotifAdapter : NotifRVAdapter
-    val datas = mutableListOf<Notif_Multi>()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentNotifBinding.inflate(inflater, container, false)
-
-        datas.apply {
-            add(Notif_Multi(R.layout.item_notif_1))
-            add(Notif_Multi(R.layout.item_notif_2))
-            add(Notif_Multi(R.layout.item_notif_3))
-            NotifAdapter.datas = datas
-            NotifAdapter.notifyDataSetChanged()
-        }
-
-        binding.notifRv.adapter=NotifAdapter //리사이클러뷰 어댑터와 연결
-        binding.notifRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-
-
-        return binding.root
-    }
-
-}*/
